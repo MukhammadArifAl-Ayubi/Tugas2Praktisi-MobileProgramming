51f9c7bac9bace23ade91a6c523e288cebfff02d
afif <afifayubi80@gmail.com>
2022-10-28 23:26:14 +0700

    Tugas Praktisi 2

---
+++ Tugas2ActionBar/app/src/main/java/com/example/tugas2actionbar/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.tugas2actionbar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu:Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_menu1-> Toast.makeText(this, "Anda Menekan Menu1", Toast.LENGTH_SHORT).show()
+            R.id.action_menu2 -> Toast.makeText(this, "Anda Menekan Menu2", Toast.LENGTH_SHORT).show()
+        }
+        return true
+    }
+}
